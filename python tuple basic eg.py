b6324d25d499635d822c18252fb7d8756b615776
Deepak <deepakms.real@gmail.com>
2018-03-12 00:08:32 +0530

    Add files via upload

---
+++ python tuple basic eg.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 11 08:01:53 2018
+
+@author: deepak
+"""
+
+first_tuple = (12, 'jack' , 45.6, 'new', (3.2), 'test')
+first_tuple[1:4]
+
+first_list = ['mark', 101,23.6,'test']
+first_pop.pop['mark']
