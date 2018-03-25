d517a4aed928417ddd83dbb0ac6c8d6e3007cdb4
Deepak <deepakms.real@gmail.com>
2018-03-25 22:58:59 +0530

    Add files via upload

---
+++ bubblesort_secondlargestnumber.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 22:54:38 2018
+
+@author: deepak
+"""
+
+a=[]
+n=int(input("Enter number of elements:"))
+for i in range(1,n+1):
+    b=int(input("Enter element:"))
+    a.append(b)
+for i in range(0,len(a)):
+    for j in range(0,len(a)-i-1):
+        if(a[j]>a[j+1]):
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp 
+print('Second largest number is:',a[n-2])
