f6e461237c7a850423ed9c6440990758ce74cbc7
Deepak <deepakms.real@gmail.com>
2018-03-25 22:58:35 +0530

    Add files via upload

---
+++ dict_for.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 20:58:16 2018
+
+@author: deepak
+"""
+
+c = { ‘a’ : 3, ‘name’: ‘Dave’, ‘x’: 7.5 }
+# Print out all of the members of a dictionary
+for key in c:
+print key, c[key]
